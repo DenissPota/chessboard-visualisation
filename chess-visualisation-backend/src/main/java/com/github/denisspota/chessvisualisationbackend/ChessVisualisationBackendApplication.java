f1d0443957e621ec47f30e7239d67145dd9b5c4e
DenissPota <dennispota@gmail.com>
2020-09-12 18:21:17 +0300

    init project

---
+++ chess-visualisation-backend/src/main/java/com/github/denisspota/chessvisualisationbackend/ChessVisualisationBackendApplication.java
@@ -0,0 +1,13 @@
+package com.github.denisspota.chessvisualisationbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessVisualisationBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChessVisualisationBackendApplication.class, args);
+	}
+
+}
---
+++ chess-visualisation-backend/src/test/java/com/github/denisspota/chessvisualisationbackend/ChessVisualisationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.denisspota.chessvisualisationbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessVisualisationBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
