056857c5599c93d27b7173bdf31f00ebfd1dca11
DenissPota <dennispota@gmail.com>
2020-09-19 14:51:45 +0300

    Add infinite puzzles functionality

--- chess-visualisation-frontend/src/app/chessboard/chessboard.component.ts
+++ chess-visualisation-frontend/src/app/chessboard/chessboard.component.ts
@@ -2,45 +2,46 @@ import {AfterViewInit, Component, Input, OnInit} from '@angular/core';
 import * as ChessBoard from 'chessboardjs/www/js/chessboard';
 import {BoardPositionService} from "../../service/board-position.service";
 import {BoardPosition} from "../interface/board-position";
-import {interval, Observable} from 'rxjs';
-import {map} from 'rxjs/operators'
+import {timeInterval} from "rxjs/operators";
 
 @Component({
   selector: 'app-chessboard',
   templateUrl: './chessboard.component.html',
   styleUrls: ['./chessboard.component.css']
 })
-export class ChessboardComponent implements AfterViewInit {
+export class ChessboardComponent implements OnInit {
 
-  private boardPositionService: BoardPositionService;
-  board1: ChessBoard;
-  guessingPhase: boolean = false;
-  winning: String;
-  currentBoardPosition: BoardPosition;
   @Input()
   seconds: number;
+
+  private boardPositionService: BoardPositionService;
+
+  board1: ChessBoard;
+  guessingPhase: boolean = false;
+  isGuessCorrect: boolean;
+  currentBoardPosition: BoardPosition;
+  initialTime: number;
+  interval: any;
 
   constructor(boardPositionService: BoardPositionService) {
     this.boardPositionService = boardPositionService;
   }
 
   ngOnInit(): void {
+    this.initialTime = this.seconds;
     this.getRandomPosition();
     this.decreaseTimer()
-  }
-
-  ngAfterViewInit() {
-    this.board1 = ChessBoard('board1', {
+    this.board1 = new ChessBoard('board1', {
       draggable: true,
       position: this.currentBoardPosition.fen
     });
   }
 
   decreaseTimer() {
-    const interval = setInterval(() => {
+    this.interval = setInterval(() => {
       this.seconds--;
       if (this.seconds === 0) {
-        clearInterval(interval);
+        clearInterval(this.interval);
         this.clearBoard();
       }
     }, 1000)
@@ -55,8 +56,10 @@ export class ChessboardComponent implements AfterViewInit {
     this.guessingPhase = true;
     this.board1.clear();
 
-    this.board1 = ChessBoard('board1', {
+    this.board1.destroy();
+    this.board1 = new ChessBoard('board1', {
       draggable: true,
+      dropOffBoard: 'trash',
       sparePieces: true,
     });
   }
@@ -64,11 +67,17 @@ export class ChessboardComponent implements AfterViewInit {
   compareGuess() {
     console.log(this.currentBoardPosition.fen);
     console.log(this.board1.fen());
-    if (this.currentBoardPosition.fen === this.board1.fen()) {
-      this.winning = "You are correct!"
-    } else {
-      this.winning = "Wrong!"
-    }
+    this.isGuessCorrect = this.currentBoardPosition.fen === this.board1.fen();
+  }
+
+  restartComponent() {
+    clearInterval(this.interval)
+    this.seconds = this.initialTime;
+    this.guessingPhase = false;
+    this.isGuessCorrect = undefined;
+    this.currentBoardPosition = undefined;
+    this.board1 = undefined;
+    this.ngOnInit()
   }
 
 }
